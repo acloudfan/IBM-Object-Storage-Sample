974b5254ffbfb2af1c3d6e8d50117aed2b36b75a
rajrin@yahoo.com <raj@acloudfan.com>
2015-12-21 06:56:12 -0500

    first commit

---
+++ src/com/acloudfan/cos/test/TestCos.java
@@ -0,0 +1,14 @@
+package com.acloudfan.cos.test;
+
+/**
+ * 
+ * @author acloudfan.com
+ * 
+ * For testing the cloud storage.
+ */
+public class TestCos {
+
+	public static void main(String[] args){
+		
+	}
+}
---
+++ src/com/acloudfan/cos/util/OpenstackStorage.java
@@ -0,0 +1,78 @@
+package com.acloudfan.cos.util;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+import org.openstack4j.api.OSClient;
+import org.openstack4j.model.common.Identifier;
+import org.openstack4j.model.common.Payload;
+import org.openstack4j.model.common.Payloads;
+import org.openstack4j.model.compute.ActionResponse;
+import org.openstack4j.openstack.OSFactory;
+
+/**
+ * @author acloudfan.com
+ * Utility object for carrying out CRUD on the openstack storage.
+ */
+public class OpenstackStorage {
+
+	final	private	OSClient	os;
+	
+	public	OpenstackStorage(String url, String userId, String password, String project, String domainName)
+	throws Exception {
+		
+		Identifier domainIdentifier = Identifier.byName(domainName);
+		Identifier projectIdentifier= Identifier.byName(project);
+		
+		os = OSFactory.builderV3()
+				.endpoint(url)
+				.credentials(userId, password)
+				.scopeToProject(projectIdentifier, domainIdentifier)
+				.authenticate();
+	}
+	
+	/**
+	 * Returns the list of containers under the account
+	 */
+	public	List containers(){
+		return os.objectStorage().containers().list();
+	}
+	
+	/**
+	 * Returns the list of objects under the account->container
+	 */
+	public	List objects(String container){
+		return os.objectStorage().objects().list(container);
+	}
+	
+	/**
+	 * Create a container
+	 */
+	public	ActionResponse	createContainer(String name){
+		return os.objectStorage().containers().create(name);
+	}
+	
+	/**
+	 * Delete a container
+	 */
+	public	ActionResponse	deleteContainer(String name){
+		return os.objectStorage().containers().delete(name);
+	}
+	
+	/**
+	 * Create an object in a container - file path
+	 */
+	public String createObject(String container, String obj, String filePath){
+		Payload<File> payload = Payloads.create(new File(filePath));
+		return os.objectStorage().objects().put(container, obj, payload);
+	}
+	
+	/**
+	 * Create an object in a container - file path
+	 */
+	public String createObject(String container, String obj, InputStream stream){
+		Payload<InputStream> payload = Payloads.create(stream);
+		return os.objectStorage().objects().put(container, obj, payload);
+	}
+}
